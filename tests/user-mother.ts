b5fca225cda9857389a520ab886af45345cc2b47
Albert Hernandez <alberthernandezdev@gmail.com>
2023-01-26 21:58:34 +0100

    feat: add faker

--- tests/user-mother.ts
+++ tests/user-mother.ts
@@ -1,19 +1,31 @@
+import { faker } from "@faker-js/faker";
+
 import { User } from "../src/user";
 
 export class UserMother {
-  static create({ id = "1", name = "juan", totalItemsPurchased = 5 }): User {
+  static create({
+    id = faker.datatype.uuid(),
+    name = faker.name.fullName(),
+    totalItemsPurchased = faker.datatype.number(),
+  }): User {
     return new User(id, name, totalItemsPurchased);
   }
 
   static withGoldLevel() {
-    return this.create({ totalItemsPurchased: 100 });
+    return this.create({
+      totalItemsPurchased: faker.datatype.number({ min: 100 }),
+    });
   }
 
   static withSilverLevel() {
-    return this.create({ totalItemsPurchased: 55 });
+    return this.create({
+      totalItemsPurchased: faker.datatype.number({ min: 50, max: 99 }),
+    });
   }
 
   static withBasicLevel() {
-    return this.create({ totalItemsPurchased: 3 });
+    return this.create({
+      totalItemsPurchased: faker.datatype.number({ max: 49 }),
+    });
   }
 }
--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,18 +1,26 @@
+import { faker } from "@faker-js/faker";
+
 import { UserMother } from "./user-mother";
 
 describe("User", () => {
   it("when the user has purchased at least 100 items is gold level", () => {
-    const user = UserMother.create({ totalItemsPurchased: 100 });
+    const user = UserMother.create({
+      totalItemsPurchased: faker.datatype.number({ min: 100 }),
+    });
     expect(user.isGoldLevel()).toBeTruthy();
   });
 
   it("when the user has purchased between 50 and 99 items is silver level", () => {
-    const user = UserMother.create({ totalItemsPurchased: 55 });
+    const user = UserMother.create({
+      totalItemsPurchased: faker.datatype.number({ min: 50, max: 99 }),
+    });
     expect(user.isSilverLevel()).toBeTruthy();
   });
 
   it("gold users should not be silver users", () => {
-    const user = UserMother.create({ totalItemsPurchased: 100 });
+    const user = UserMother.create({
+      totalItemsPurchased: faker.datatype.number({ min: 100 }),
+    });
     expect(user.isGoldLevel()).toBeTruthy();
     expect(user.isSilverLevel()).toBeFalsy();
   });
